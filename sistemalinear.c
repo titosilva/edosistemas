bdb1a1d831848d964d069740700a530a6011c63a
titosilva <jt.mat@hotmail.com>
2019-05-11 20:37:13 -0300

    Parte de soluções com autovalores complexos pronta

--- sistemalinear.c
+++ sistemalinear.c
@@ -7,16 +7,21 @@ int solucionarSistema2x2(Matriz2x2 mat){
     a2 = autovalor2x2_2(mat);
 
     if(a1.imaginario==0 && a2.imaginario==0){
+        if(a1.real==a2.real){
+
+        }
 
     }else{
         printf("Sistema do tipo z'(t) = Az(t)\n");
         printf("Autovalores complexos! %lf+i(%lf) e %lf+i(%lf)\n", a1.real, a1.imaginario, a2.real, a2.imaginario);
-        printf("Soluções:\n");
-        printf("z(t) = (e^%lft)( cos(%lft)u - sen(%lft)w ) + (e^%lft)( cos(%lft)w + sen(%lft)u )\n", 
-               a1.real, a1.imaginario, a1.imaginario, a1.real, a1.imaginario, a1.imaginario);
-        printf("Onde w = (0, k*%lf) e u=(k, k*%lf)\n", a1.imaginario/mat.valores[0][1],
-                                                    (a1.real-mat.valores[0][0])/mat.valores[0][1] );
-        printf("Com k sendo uma constante que depende dos valores iniciais\n");
+        printf("\nSoluções:\n");
+        printf("z1(t) = (e^%lft)( cos(%lft)u - sen(%lft)w )\n", a1.real, a1.imaginario, a1.imaginario);
+        printf("z2(t) = (e^%lft)( cos(%lft)w + sen(%lft)u )\n", a1.real, a1.imaginario, a1.imaginario);
+        printf("Onde u e w são, respectivamente, a parte real e a parte imaginaria do autovetor associado a um dos\
+                autovalores\n");
+        printf("Tomando o autovalor de parte imaginaria positiva, temos:\n");
+        printf("u=(k,%lf*k)\nw=(0,%lf*k)\n", (a1.real-mat.valores[0][0])/mat.valores[0][1], (a1.imaginario)/mat.valores[0][1]);
+        printf("Para algum k real, que pode ser determinado por meio das condições iniciais do sistema\n");
         return -1;
     }
 }
