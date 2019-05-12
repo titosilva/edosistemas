ee7d9b700a88e581da3b22944424cfe8401c67f6
titosilva <jt.mat@hotmail.com>
2019-05-12 20:50:26 -0300

    Quase pronto!

--- sistemalinear.c
+++ sistemalinear.c
@@ -8,9 +8,30 @@ int solucionarSistema2x2(Matriz2x2 mat){
 
     if(a1.imaginario==0 && a2.imaginario==0){
         if(a1.real==a2.real){
-
+            if(mat.valores[1][0]==0 && mat.valores[0][1]==0){
+                printf("Sistema do tipo z'(t) = Az(t)\n");
+                printf("Autovalores iguais e matriz A=kI com k sendo uma constante real\n");
+                printf("\nSoluções:\n");
+                printf("z(t)=(k1 e^(%lft), k2 e^(%lft))", mat.valores[0][0], mat.valores[0][0]);
+                printf("Onde k1 e k2 são números reais tais que z(0)=(k1,k2)\n");
+            }else{
+                printf("Sistema do tipo z'(t) = Az(t)\n");
+                printf("Autovalores iguais e dim Nuc(A-aI) = 1, com a sendo autovalor de A\n");
+                printf("\nSoluções:\n");
+                printf("z(t)=e^(%lft)*(u1+(k1t+k2)v1, u2+(k1t+k2)v2))\n");
+                printf("Onde v=(v1,v2) é autovetor da matriz A e u=(u1,u2) é tal que v=(A-aI)u\n");
+                printf("k1 e k2 são constantes que dependem das condições iniciais\n");
+                printf("Assim, v=(c1, %lf*c1) e u=(c2, (c1-c2(%lf))/%lf)", (a1.real-mat.valores[0][0])/mat.valores[0][1],
+                       );
+            }
+        }else{
+            printf("Sistema do tipo z'(t) = Az(t)\n");
+            printf("Autovalores reais e diferentes\n");
+            printf("\nSoluções:\n");
+            printf("z(t)=(u1 e^(%lft) + v1 e^(%lft), u2 e^(%lft) + v2 e^(%lft))\n",a1.real,a2.real,a1.real, a2.real);
+            printf("Onde u=(u1,u2) e v=(v1,v2) são autovetores associados aos autovalores da matriz\n");
+            printf("Temos, portanto, u2=%lf*u1 e v2=%lf*v1\n", (a1.real-mat.valores[0][0])/mat.valores[0][1], (a2.real-mat.valores[0][0])/mat.valores[0][1]);
         }
-
     }else{
         printf("Sistema do tipo z'(t) = Az(t)\n");
         printf("Autovalores complexos! %lf+i(%lf) e %lf+i(%lf)\n", a1.real, a1.imaginario, a2.real, a2.imaginario);
